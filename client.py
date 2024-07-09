27b4f33cb3a48453ff5efb5b6a04662880098045
ernatan2 <ernatan2@gmail.com>
2024-07-09 19:46:56 +0300

    the now server can handle multiple clients

--- client.py
+++ client.py
@@ -2,15 +2,50 @@ import socket
 import time
 
 SERVER_ADDRESS = '127.0.0.1' , 54321
-
-
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(SERVER_ADDRESS)
 
-while True:
-    with open("status.txt") as f:
-        message = f.read()
-        message_data = message.encode('utf8')
-        client_socket(message_data)
 
-    time.sleep(60)
+try:
+    client_socket.connect(SERVER_ADDRESS)
+    print("connected to: {}" .format(SERVER_ADDRESS[0]))
+
+    while True:
+        massage  = input("input message: ")
+        massege_data = massage.encode('utf8')
+        client_socket.send(massege_data)
+
+        response_data = client_socket.recv(1024)
+        response_text = response_data.decode('utf8')
+        print("Response", response_text)
+
+
+except ConnectionRefusedError:
+    print("faild to connect to the server")
+except KeyboardInterrupt:
+    print("\nclient shutting down")
+except Exception as e:
+    print("An error occured: {}" .format(e))
+
+finally:
+    client_socket.close()
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # with open("status.txt") as f:
+        # message = f.read()
+        # message_data = message.encode('utf8')
+        # client_socket(message_data)
+
+    #time.sleep(60)
--- server.py
+++ server.py
@@ -1,33 +1,70 @@
 import socket
 import sqlite3
 import time
+import threading
 
 
 #creating the connection with sqlite3 and creating the table
 
-with sqlite3.connect('water_station.sqlite3') as conn:
-    cur = conn.cursor()
-    cur.execute('''
-                CREATE TABLE IF NOT EXISTS station_status (
-                        station_id INT,
-                        last_date TEXT,
-                        alarm1 INT,
-                        alarm2 INT,
-                        PRIMARY KEY(station_id) ); 
-                ''')
+# with sqlite3.connect('water_station.sqlite3') as conn:
+#     cur = conn.cursor()
+#     cur.execute('''
+#                 CREATE TABLE IF NOT EXISTS station_status (
+#                         station_id INT,
+#                         last_date TEXT,
+#                         alarm1 INT,
+#                         alarm2 INT,
+#                         PRIMARY KEY(station_id) ); 
+#                 ''')
 
 
 #creating the server connection
 
 SERVER_ADDRESS = '127.0.0.1' , 54321
 
-accept_socket = socket.socket()
-accept_socket.bind(SERVER_ADDRESS)
-accept_socket.listen(20)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(SERVER_ADDRESS)
+server_socket.listen(16)
 
 print("server is listening...")
 
-while True:
+def client_handle(client_conn, client_addr):
+    try:
+        print("new client connected: {}: {}" .format(*client_adrr))
+        
+        while True:
+            client_data = client_conn.recv(1024) 
 
+            if not client_data:
+                break   
+
+            print('received: "{}"'.format(client_data.decode('utf8')))
+
+            response = "message ressived"
+
+            client_conn.send(response.encode('utf8'))
+    
+    except Exception as e:
+        print("error with the client{}" .format(client_addr, e))
+    
+    finally:
+        client_conn.close()
+
+
+
+try:
+    while True:
+        client_conn, client_adrr = server_socket.accept()
+        client_threading = threading.Thread(target = client_handle, args = (client_conn, client_adrr))
+        client_threading.start()
+except KeyboardInterrupt:
+    print("\nserver shutting down")
+finally:
+    server_socket.close()
+
+
+
+
+    
 
     
